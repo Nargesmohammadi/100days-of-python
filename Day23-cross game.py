1214d1dd7c9f01fb1a22546c3da8da41243bd6a7
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-01 12:41:35 +0430

    create the screen for crossing game.

---
+++ Day23-cross game.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+import time
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+screen.title("Cross")
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
