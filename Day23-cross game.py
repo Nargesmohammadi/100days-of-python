fab2ea2862a2441acb3c1776496591ab0dd48780
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-01 17:21:50 +0430

    create two new objects and use the listen method for screen and control the turtle's moving by player with up key.

--- Day23-cross game.py
+++ Day23-cross game.py
@@ -1,18 +1,29 @@
 from turtle import Turtle, Screen
+from player import Player
+from carmanager import CarManager
+from scoreboardd import Scoreboard
 import time
 
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=600, height=600)
-screen.title("Cross")
+screen.title("Crossing game")
 screen.tracer(0)
+
+player = Player()
+car = CarManager()
+
+
+screen.listen()
+screen.onkey(player.up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
+    car.cars()
+    car.move()
 
 
 
