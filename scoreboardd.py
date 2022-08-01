e163e3a585ce41cc3901b6d332c3d85114b9b008
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-01 17:33:18 +0430

    In scoreboard class created methods for writing number of levels in every level and print the game over in the center if player lose.

--- scoreboardd.py
+++ scoreboardd.py
@@ -4,5 +4,24 @@ from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(270, 250)
+        self.update_board()
+
+    def update_board(self):
+        self.clear()
+        print(self.level)
+        self.write(f"level: {self.level}", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_board()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game over", align="center", font=FONT)
