fd13ef05b75a1a4e1ddeb00dc5b338d103e64b4e
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-01 18:01:43 +0430

    changing.

--- carmanager.py
+++ carmanager.py
@@ -4,31 +4,35 @@ import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-Y = (-250, 250)
 
 
 class CarManager:
     def __init__(self):
         self.all_cars = []
-        self.count = 0
+        # self.count = 0
         self.speed = STARTING_MOVE_DISTANCE
         self.cars()
 
     def cars(self):
-        self.count += 1
-        if self.count % 6 == 0:
+        # self.count += 1
+        # if self.count % 6 == 0:
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
             new_car = Turtle()
-            new_car.penup()
-            new_car.color = random.choice(COLORS)
             new_car.shape("square")
+            # cars are on right shape
             new_car.shapesize(stretch_wid=1, stretch_len=2)
-            new_car.goto(290, random.choice(Y))
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            y = random.randint(-250, 250)
+            new_car.goto(300, y)
             self.all_cars.append(new_car)
 
     def move(self):
         for car in self.all_cars:
-            x = car.xcor() - self.speed
-            car.goto(x, car.ycor())
+            # x = car.xcor() - self.speed
+            # car.goto(x, car.ycor())
+            car.backward(STARTING_MOVE_DISTANCE)
 
     def speed_up(self):
         self.speed += MOVE_INCREMENT
