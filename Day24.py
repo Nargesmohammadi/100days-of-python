c5001b86e1f848fb5c72c2cc7e955ee4c219bb8b
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-02 19:53:29 +0430

    absolute and relative the files.

--- Day24.py
+++ Day24.py
@@ -1,12 +1,13 @@
-# file = open("my_file.txt")
 # open and read the file.
-with open("my_file.txt") as file:
+# absolute
+with open("/home/mitraa/Desktop/my_file.txt") as file:
     contents = file.read()
     print(contents)
 
+# relative
+with open("../../Desktop/my_file.txt") as f:
+    contents = f.read()
+    print(contents)
 
-# open and write the file, use append in mod=
-with open("new_file.txt", mode="a") as file:
-    file.write("\nNew text.")
 
 
