98dd6afe46848c3dbeb94359d8286f44729f3549
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-02 19:20:12 +0430

    open and read the file. open and write the file, use append in mod=

--- Day24.py
+++ Day24.py
@@ -1,4 +1,12 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+# file = open("my_file.txt")
+# open and read the file.
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+
+# open and write the file, use append in mod=
+with open("new_file.txt", mode="a") as file:
+    file.write("\nNew text.")
+
+
