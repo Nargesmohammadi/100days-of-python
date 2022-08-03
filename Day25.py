96e42731e0026537a3fff049deb43db4237f53cd
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-03 13:28:03 +0430

    reading csv data.

---
+++ Day25.py
@@ -0,0 +1,15 @@
+# with open("./002 weather-data.csv") as weathers_file:
+    # weather = weathers_file.readlines()
+    # print(weather)
+
+
+import csv
+
+with open("./002 weather-data.csv") as data_file:
+    data = csv.reader(data_file)
+    temperature = []
+    for row in data:
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
+    print(temperature)
+
