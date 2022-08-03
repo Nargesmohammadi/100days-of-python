1551734d7d78088657c316e9636866f8690b2191
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-03 21:34:40 +0430

    practice

--- Day26/Day26-1.py
+++ Day26/Day26-1.py
@@ -14,3 +14,19 @@ names2 = ['Alex', 'Caroline', 'David', 'Ali']
 long_name = [name.upper() for name in names2 if len(name) > 5]
 print(long_name)
 
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+new_number = [num * num for num in numbers]
+print(new_number)
+
+new_number1 = [num for num in numbers if num % 2 == 0]
+print(new_number1)
+
+with open("file1.txt") as file1:
+    file_1_data = file1.read()
+
+with open("file2.txt") as file2:
+    file_2_data = file2.read()
+
+result = [int(num) for num in file_1_data if num in file_2_data]
+
+
