67410c3c913c6bd830271d9082d1379314e09a9a
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-04 21:58:31 +0430

    Tkinter layout manager pack(), place() and grid().

--- Day27/Day27-1.py
+++ Day27/Day27-1.py
@@ -1,13 +1,40 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
 
-
 # label:
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+
+# changing the label's property
+my_label["text"] = "new text"
+my_label.config(text="new text")
+# replace the label:
+# my_label.place(x=0, y=0)
+my_label.grid(column=0, row=0)
+
+# button
+
+def button_clicked():
+    print("I got clicked.")
+    new_text = input.get()
+    # when we click on button, label changes the text:
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click me", command=button_clicked)
+button.pack(side="left")
+
+# entry
+input = Entry(width=10)
+input.pack(side="left")
+print(input.get())
+# return the input as string by get() method:
+
+
+
 
 
 
