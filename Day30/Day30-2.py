6351e2c544d86b5e676342343c4abd573d2609fe
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-07 17:36:44 +0430

    exercise exception handing in the NATO phonetic project.

--- Day30/Day30-2.py
+++ Day30/Day30-2.py
@@ -1,4 +1,3 @@
-
 fruits = ["Apple", "Pear", "Orange"]
 
 
@@ -30,3 +29,30 @@ for post in facebook_posts:
         pass
 
 print(total_likes)
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("./Day30/nato_phonetic_alphabet.csv")
+# TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+# for continuing the code:
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
