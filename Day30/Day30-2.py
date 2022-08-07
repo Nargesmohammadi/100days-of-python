9822c81bc814c995a9940a8718dd5c14ce66adce
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-08-07 17:22:35 +0430

    key error handing.

--- Day30/Day30-2.py
+++ Day30/Day30-2.py
@@ -13,3 +13,20 @@ def make_pie(index):
 
 make_pie(4)
 
+facebook_posts = [
+    {'likes': 21, 'comments': 2},
+    {'likes': 13, 'comments': 2, 'shares': 1},
+    {'likes': 33, 'comments': 8, 'shares': 3},
+    {'comments': 4, 'shares': 2},
+    {'comments': 1, 'shares': 1},
+    {'likes': 19, 'comments': 3}
+]
+total_likes = 0
+
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['likes']
+    except KeyError:
+        pass
+
+print(total_likes)
